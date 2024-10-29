f6c83ebbfbc55704a974e840ea7a4923feb59548
Guilherme Rozenblat <gui.rozenblat@gmail.com>
2024-10-29 04:46:24 -0300

    29/10versao2

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css"; // Certifique-se de criar este arquivo CSS
-
 import video4 from "../../assets/video4.mp4";
 
-
 function Home() {
+  useEffect(() => {
+    // Define a altura do conteúdo para excluir a barra de navegação no mobile
+    const setAppHeight = () => {
+      const doc = document.documentElement;
+      doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+    };
+
+    // Define a altura ao carregar a página e ao redimensionar a tela
+    setAppHeight();
+    window.addEventListener("resize", setAppHeight);
+
+    return () => window.removeEventListener("resize", setAppHeight);
+  }, []);
+
   return (
     <section id="home" className="home">
       <video autoPlay loop muted className="background-video">
@@ -18,9 +30,7 @@ function Home() {
           <h1>Rozenblat</h1>
           <h2 className="text2">Full-Stack Developer</h2>
         </div>
-        
       </div>
-      */
     </section>
   );
 }
